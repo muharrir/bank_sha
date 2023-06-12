b84a8a69ea2f6a0fb58fb3d574b59475ef6331dc
Muharrir <muharrir97@gmail.com>
2023-06-12 09:55:52 +0700

    added signin page, and signup page

---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+import '../widgets/buttons.dart';
+import '../widgets/forms.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightBackgroundColor,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          Container(
+            width: 155,
+            height: 50,
+            margin: const EdgeInsets.symmetric(vertical: 100),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_logo_light.png',
+                ),
+              ),
+            ),
+          ),
+          Text(
+            'Join Us to Unlock\nYour Growth',
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //NOTE: NAME INPUT
+                const CustomFormField(
+                  title: 'Full Name',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                //NOTE: EMAIL INPUT
+                const CustomFormField(
+                  title: 'Email Address',
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                //NOTE: PASSWORD INPUT
+                const CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Continue',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/sign-up-set-profile');
+                  },
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextButton(
+            title: 'Sign In',
+            onPressed: () {
+              Navigator.pushNamed(context, '/sign-in');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
